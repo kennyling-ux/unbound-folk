9c1481418fd4703581e366ed6abfd0f0667054ac
unboundfolk <hello@unboundfolk.com>
2026-04-12 00:54:33 +0800

    update sitemap change frequency

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://unboundfolk.com",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "monthly",
       priority: 1,
     },
   ];
